86e5972630337611623106c694c0bd984ba83b06
Nitin Kumar <acinonyx219@gmail.com>
2023-10-07 13:28:51 +0530

    added models and schemas

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const connect = require('./config/database');
 
+const Tweet = require('./models/tweet');
+const TweetRepository = require('./repository/tweet-repository');
+
 app.listen(3000, async () => {
     "server started..."
     await connect();
     console.log('mongodb connected successfully....')
+    // const tweet = await Tweet.create({
+    //     content: "hello world",
+    //     userEmail: 'a@gmail.com'
+    // });
+
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.create({content: 'Hello world', userEmail: "a@gmail.com"});
+    console.log(tweet);
 })
