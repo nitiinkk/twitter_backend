663c629547baf9dcaa341fe9a2e8497a44133c83
nitiiink1 <nitiiink1@gmail.com>
2023-10-14 16:56:22 +0530

    added signup with encrypted password

--- src/repository/index.js
+++ src/repository/index.js
@@ -3,11 +3,13 @@ import HashTagRepository from './hashtag-repository';
 import LikeRepository  from './like-repository.js';
 import UserRepository from './user-repository.js';
 import CommentRepository from "../repository/comment-repository";
+import UserRepository from '../repository/user-repository.js';
 
 export {
     TweetRepository,
     HashTagRepository,
     LikeRepository,
     UserRepository,
-    CommentRepository
+    CommentRepository,
+    UserRepository
 }
---
+++ src/services/user-service.js
@@ -0,0 +1,20 @@
+import { UserRepository } from '../repository/user-repository.js';
+
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async signup(data) {
+        try {
+            const user = await this.userRepository.create(data);
+            return user;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+}
+
+export default UserService;
